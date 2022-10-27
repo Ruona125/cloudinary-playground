6a9ff8b3a67b7306da90e83cbe4fc0a3015f0ee6
ruona <ogheneruonaagadagba4@gmail.com>
2022-10-27 15:05:44 +0100

    finished up everything

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Image } from "cloudinary-react";
 
 export default function Home() {
+  const [imageIds, setImageIds] = useState();
+  const loadImages = async () => {
+    try {
+      let res = await axios.get("/api/images").then((res) => {
+        console.log(res.data);
+        setImageIds(res.data);
+      });
+      //   const data = await res.json();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    loadImages();
+  }, []);
   return (
     <div>
       <p>this is the home page</p>
+      {imageIds &&
+        imageIds.map((imageId, index) => (
+          <Image
+            key={index}
+            cloudName="dz7pivxws"
+            publicId={imageId}
+            width="300"
+            crop="scale"
+          />
+        ))}
     </div>
   );
 }
